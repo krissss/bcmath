c8215d2a5092492e9b9c5a84e13d758c71e0f869
kriss <462679766@qq.com>
2024-06-26 13:09:33 +0800

    feature: 对于非科学计数法的数字，不转 number_format

--- tests/IssueTest.php
+++ tests/IssueTest.php
@@ -3,7 +3,7 @@
 use kriss\bcmath\BC;
 use kriss\bcmath\BCS;
 
-test('issues2', function () {
+test('issue-2', function () {
     $total = BCS::create(0, ['scale' => 6]);
     $total->add(1);
     $total->add(0.000008);
@@ -20,7 +20,7 @@ test('issues2', function () {
     expect($total->getResult())->toEqual(1000.000008);
 });
 
-test('issues7', function () {
+test('issue-7', function () {
     $result = BC::create(['scale' => 2])->compare('1e3', 100);
     expect($result)->toEqual(1);
 
@@ -32,4 +32,14 @@ test('issues7', function () {
 
     $result = BCS::create(123)->compare('1e3');
     expect($result)->toEqual(-1);
+});
+
+test('issue-9', function () {
+    $result = BCS::create((string)1233123131432143214321412.123)->compare((string)1233123131432143214321411.123);
+    expect($result)->toEqual(0);
+    $result = BCS::create(1233123131432143214321412.123)->compare(1233123131432143214321411.123);
+    expect($result)->toEqual(0);
+
+    $result = BCS::create('1233123131432143214321412.123')->compare('1233123131432143214321411.123');
+    expect($result)->toEqual(1);
 });
